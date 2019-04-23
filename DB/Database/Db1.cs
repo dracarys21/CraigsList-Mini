1a6133f2e4dfa0c44a5c0bb91c04cb1fa3c8e1ac
Christian Fernandez <fernandezc@mercury.law.nyu.edu>
2019-04-22 21:57:32 -0400

    Set up starter project

---
+++ DB/Database/Db1.cs
@@ -0,0 +1,50 @@
+﻿using Data.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BizLogic.Logic;
+
+namespace DB.Database
+{
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+
+    public class Db1
+    {
+
+        public static void DoDatabaseOperation()
+        {
+            // You can place your models into ApplicationDbContext
+            // or create your own context
+            using (var db = new ApplicationDbContext())
+            {
+                // Create and manipulate your model
+                Example ex = new Example();
+                ex.Name = "Hello";
+
+                // Call into biz logic
+                Biz1.IsExampleValid(ex);
+
+                // Do something useful here
+
+                db.SaveChanges();
+            }
+        }
+
+    }
+}
